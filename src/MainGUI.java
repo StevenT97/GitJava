f4ffa03d63ec009ac2c35f0469ad31cb8f4c9800
StevenT97 <thuannguyen.adp@gmail.com>
2016-11-02 15:39:39 +0700

    init

---
+++ src/MainGUI.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class MainGUI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
